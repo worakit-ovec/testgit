67c3db28808a1a8456304914c2a697defbb70a17
worakit-ovec <worakit.wi@ovec.moe.go.th>
2021-01-15 14:38:53 +0700

    edit index

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ echo "My Hompage";
 ?>
 <a href="page1.php">page1</a>
 <a href="page2.php">page2</a>
-<a href="page3.php">page3</a>
+<a href="page3.php">page3</a> 
+<a href="page4.php">page4</a> 
+<a href="page5.php">page5</a>  
